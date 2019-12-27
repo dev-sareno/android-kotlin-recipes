196faa23501b8ed261317933aa3200eb82ed5bc4
Jay-ar Sareno <eladio.sareno@netzon.com.se>
2019-12-27 11:36:11 +0800

    Added: Suspend 1 example

---
+++ app/src/main/java/com/netzon/androidkotlinrecipes/coroutines/CoroutinesActivity.kt
@@ -0,0 +1,22 @@
+package com.netzon.androidkotlinrecipes.coroutines
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.netzon.androidkotlinrecipes.R
+import kotlinx.android.synthetic.main.activity_coroutines.*
+
+class CoroutinesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutines)
+
+        this.title = "Coroutines"
+
+        btn_suspend1.setOnClickListener {
+            val intent = Intent(this, Suspend1Activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
